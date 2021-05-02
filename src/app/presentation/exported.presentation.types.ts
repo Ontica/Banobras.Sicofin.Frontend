8c7b1869ced9d3819e5a43d367d0295d79e3298c
José Manuel Cota <jmcota@ontica.org>
2021-05-01 23:30:42 -0400

    Initial presentation and data services modules added

---
+++ src/app/presentation/exported.presentation.types.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+
+import { MainLayoutActions, MainLayoutSelectors } from './main-layout/_main-layout.presentation.types';
+export * from './main-layout/_main-layout.presentation.types';
+
+import { FAActions, FACommands, FAEffects, FASelectors } from './financial-accounting/_financial.accounting.presentation.types';
+export * from './financial-accounting/_financial.accounting.presentation.types';
+
+
+/* Exportation types */
+
+export type ActionType = MainLayoutActions | FAActions;
+
+export type CommandType = FACommands;
+
+export type StateEffect = FAEffects;
+
+export type StateSelector = MainLayoutSelectors | FASelectors;
---
+++ src/app/presentation/financial-accounting/_financial.accounting.presentation.types.ts
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+
+/* Actions */
+
+export type FAActions = '';
+
+
+/* Commands */
+
+export type FACommands = '';
+
+
+/* Effects */
+
+export type FAEffects = '';
+
+
+/* Selectors */
+
+
+export type FASelectors = '';
