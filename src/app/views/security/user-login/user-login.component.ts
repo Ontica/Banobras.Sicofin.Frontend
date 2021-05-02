a6f2d3848b9dbfb22fdc8098e40875dd7460efe5
José Manuel Cota <jmcota@ontica.org>
2021-05-02 11:00:11 -0400

    Security module with user login component added

---
+++ src/app/views/security/user-login/user-login.component.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '@app/core';
+
+@Component({
+  selector: 'emp-ng-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.scss'],
+})
+export class UserLoginComponent implements OnInit {
+
+  form = new FormGroup({
+    userID: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  exceptionMsg: string;
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) { }
+
+
+  ngOnInit() {
+    this.authenticationService.logout()
+        .then((x: boolean) => this.reloadPage(x));
+  }
+
+  login() {
+    if (this.form.valid) {
+      this.authenticate();
+    }
+  }
+
+  // private methods
+
+  private authenticate(): Promise<boolean> {
+    return this.authenticationService.login(this.form.value.userID, this.form.value.password)
+      .then(
+        () => this.router.navigate(['/operacion-contable']),
+        err => this.exceptionMsg = err
+      );
+  }
+
+
+  private reloadPage(mustReload: boolean) {
+    if (mustReload) {
+      window.location.reload();
+    }
+  }
+
+}
