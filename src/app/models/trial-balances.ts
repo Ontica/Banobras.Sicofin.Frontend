7078164120e5413eb54da64b6ede8fafb31d8a1e
José Manuel Cota <jmcota@ontica.org>
2021-05-27 11:28:50 -0500

    TrialBalance model added

---
+++ src/app/models/trial-balances.ts
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+import { Identifiable } from '@app/core';
+
+
+export const TrialBalanceType: Identifiable[] = [
+  {uid: 'Traditional', name: 'Balanza tradicional'},
+  {uid: 'Valued', name: 'Balanza valorizada'}
+];
+
+
+export const BalancesType: Identifiable[] = [
+  {uid: 'AllAccounts', name: 'Todas las cuentas'},
+  {uid: 'WithCurrentBalance', name: 'Cuentas con saldo actual'},
+  {uid: 'WithCurrenBalanceOrMovements', name: 'Cuentas con saldo actual o movimientos'},
+  {uid: 'WithMovements', name: 'Cuentas con movimientos'}
+];
+
+
+export interface TrialBalanceCommand {
+  trialBalanceType: string;
+  accountsChartUID: string;
+  fromDate: string;
+  toDate: string;
+  consolidated: boolean;
+  ledgers?: string[];
+  sectors?: string[];
+  fromAccount?: string;
+  toAccount?: string;
+  level?: number;
+  balancesType?: string;
+}
+
+
+export interface TrialBalance {
+  command: TrialBalanceCommand;
+  entries: TrialBalanceEntry[];
+}
+
+
+export interface TrialBalanceEntry {
+  ledger: Identifiable;
+  currency: Identifiable;
+  accountUID: string;
+  accountNumber: string;
+  accountName: string;
+  ledgerAccountId?: number;
+  sector: Identifiable;
+  initialBalance: number;
+  debit: number;
+  credit: number;
+  currentBalance: number;
+}
