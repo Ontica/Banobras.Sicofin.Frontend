b7200a644ff4a61dc1427be384f4f49e6ed2ff49
Janeth Montero <janeth.montero@ontica.org>
2021-06-16 17:49:38 -0700

    Add export-report-modal component

---
+++ src/app/views/reports-controls/export-report-modal/export-report-modal.component.ts
@@ -0,0 +1,64 @@
+/**
+ * @license
+ * Copyright (c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved.
+ *
+ * See LICENSE.txt in the project root for complete license information.
+ */
+
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+
+import { EventInfo } from '@app/core';
+
+export enum ExportReportModalEventType {
+  CLOSE_MODAL_CLICKED = 'ExportReportModalComponent.Event.CloseModalClicked',
+  EXPORT_EXCEL_CLICKED = 'ExportReportModalComponent.Event.ExportExcelClicked',
+}
+
+@Component({
+  selector: 'emp-fa-export-report-modal',
+  templateUrl: './export-report-modal.component.html',
+})
+export class ExportReportModalComponent implements OnChanges {
+
+  @Input() title = 'Exportar';
+
+  @Input() excelFileUrl = '';
+
+  @Output() exportReportModalEvent = new EventEmitter<EventInfo>();
+
+  working = false;
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.excelFileUrl) {
+      this.working = false;
+    }
+  }
+
+
+  get hasExcelFileUrl() {
+    return !!this.excelFileUrl;
+  }
+
+
+  onClose() {
+    this.sendEvent(ExportReportModalEventType.CLOSE_MODAL_CLICKED);
+  }
+
+
+  onExportButtonClicked() {
+    this.working = true;
+    this.sendEvent(ExportReportModalEventType.EXPORT_EXCEL_CLICKED);
+  }
+
+
+  private sendEvent(eventType: ExportReportModalEventType, payload?: any) {
+    const event: EventInfo = {
+      type: eventType,
+      payload
+    };
+
+    this.exportReportModalEvent.emit(event);
+  }
+
+}
