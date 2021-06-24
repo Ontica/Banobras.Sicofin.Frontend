83628d42147a01f010adc9f3dc83fc1d68315780
Janeth Montero <janeth.montero@ontica.org>
2021-06-24 02:00:01 -0700

    Add recalculate tab and subsidiary account columns

--- src/app/views/balances-generation/stored-balance-set-tabbed-view/stored-balance-set-tabbed-view.component.ts
+++ src/app/views/balances-generation/stored-balance-set-tabbed-view/stored-balance-set-tabbed-view.component.ts
@@ -5,11 +5,20 @@
  * See LICENSE.txt in the project root for complete license information.
  */
 
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, ViewChild } from '@angular/core';
 
 import { TableVirtualScrollDataSource } from 'ng-table-virtual-scroll';
 
 import { EmptyStoredBalanceSet, StoredBalance, StoredBalanceSet } from '@app/models';
+
+import { EventInfo, Identifiable } from '@app/core';
+
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+
+export enum StoredBalanceSetTabbedViewEventType {
+  CLOSE_MODAL_CLICKED = 'StoredBalanceSetTabbedViewComponent.Event.CloseModalClicked',
+  CALCULATE_STORED_BALANCE_SET = 'StoredBalanceSetTabbedViewComponent.Event.CalculateStoredBalanceSetClicked',
+}
 
 @Component({
   selector: 'emp-fa-stored-balance-set-tabbed-view',
@@ -17,9 +26,11 @@ import { EmptyStoredBalanceSet, StoredBalance, StoredBalanceSet } from '@app/mod
 })
 export class StoredBalanceSetTabbedViewComponent implements OnChanges {
 
+  @ViewChild(CdkVirtualScrollViewport) virtualScroll: CdkVirtualScrollViewport;
+
   @Input() storedBalanceSet: StoredBalanceSet = EmptyStoredBalanceSet;
 
-  @Output() closeEvent = new EventEmitter<void>();
+  @Output() storedBalanceSetTabbedViewEvent = new EventEmitter<EventInfo>();
 
   title = 'Saldos acumulados';
 
@@ -27,14 +38,16 @@ export class StoredBalanceSetTabbedViewComponent implements OnChanges {
 
   selectedTabIndex = 0;
 
-  queryByList: any[] = ['A nivel de auxiliar'];
+  ledgerList: Identifiable[] = [];
 
-  queryBy = '';
+  selectedLedger: Identifiable = null;
+
+  showSubsidiaryAccount = false;
 
   keywords = '';
 
-  storedBalanceDisplayedColumns: string[] = ['accountNumber', 'sectorCode', 'accountName', 'partial',
-                                             'balance'];
+  storedBalanceDisplayedColumns: string[] =
+    ['ledger', 'sectorCode', 'accountNumber', 'accountName', 'balance'];
 
   storedBalanceListDS: TableVirtualScrollDataSource<StoredBalance>;
 
@@ -42,22 +55,76 @@ export class StoredBalanceSetTabbedViewComponent implements OnChanges {
   ngOnChanges() {
     this.setTitle();
     this.clearFilters();
-    this.storedBalanceListDS = new TableVirtualScrollDataSource(this.storedBalanceSet.balances ?? []);
+    this.initDataSource();
+    this.scrollToTop();
   }
 
 
   onClose() {
-    this.closeEvent.emit();
+    this.sendEvent(StoredBalanceSetTabbedViewEventType.CLOSE_MODAL_CLICKED);
+  }
+
+
+  onFilterDataClicked() {
+    this.setDisplayedColumns();
+    this.scrollToTop();
+  }
+
+
+  onCalculateStoredBalanceSetClicked() {
+    const payload = {
+      accountsChartUID: this.storedBalanceSet.accountsChart.uid,
+      balanceSetUID: this.storedBalanceSet.uid,
+      storedBalanceSet: {balancesDate: this.storedBalanceSet.balancesDate},
+    };
+
+    this.sendEvent(StoredBalanceSetTabbedViewEventType.CALCULATE_STORED_BALANCE_SET, payload);
   }
 
 
   private setTitle() {
-    this.hint = this.storedBalanceSet.name;
+    this.hint = `<strong>${this.storedBalanceSet.accountsChart.name} &nbsp; &nbsp; | &nbsp; &nbsp; </strong>`
+                + `${this.storedBalanceSet.name}`;
   }
 
+
   private clearFilters() {
-    this.queryBy = '';
+    this.selectedLedger = null;
     this.keywords = '';
+  }
+
+
+  private initDataSource() {
+    this.storedBalanceListDS = new TableVirtualScrollDataSource(this.storedBalanceSet.balances ?? []);
+    this.setDisplayedColumns();
+  }
+
+
+  private setDisplayedColumns() {
+    if (this.showSubsidiaryAccount) {
+      this.storedBalanceDisplayedColumns = ['ledger', 'sectorCode', 'accountNumber', 'accountName',
+                                            'subsidiaryAccountNumber', 'subsidiaryAccountName', 'balance'];
+    } else {
+      this.storedBalanceDisplayedColumns =
+        ['ledger', 'sectorCode', 'accountNumber', 'accountName', 'balance'];
+    }
+  }
+
+
+  private scrollToTop() {
+    if (this.virtualScroll) {
+      this.virtualScroll.scrollToIndex(-1);
+    }
+  }
+
+
+  private sendEvent(eventType: StoredBalanceSetTabbedViewEventType, payload?: any) {
+    const event: EventInfo = {
+      type: eventType,
+      payload
+    };
+
+    this.storedBalanceSetTabbedViewEvent.emit(event);
   }
 
 }
