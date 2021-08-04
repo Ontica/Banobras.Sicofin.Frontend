7d3ee44ef896d3d967f856dc3c24824992b5301d
Janeth Montero <janeth.montero@ontica.org>
2021-08-04 12:42:58 -0700

    Config the 'moment' library to show the days of week in spanish

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -51,7 +51,13 @@ import * as moment from 'moment';
 const APP_DEFAULT_DATE_LOCAL = 'es-MX';
 
 moment.updateLocale(APP_DEFAULT_DATE_LOCAL, {
+  months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'
+            .split('_'),
   monthsShort: 'Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic'.split('_'),
+  weekdays: 'Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado'.split('_'),
+  weekdaysShort: 'Dom_Lun_Mar_Mié_Jue_Vie_Sáb'.split('_'),
+  weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sá'.split('_'),
+  weekdaysParseExact: true,
   longDateFormat: {
     LT: 'HH:mm',
     LTS: 'HH:mm:ss',
